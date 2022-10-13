d4e7f3c44e1daa0a843b6351839383e02b0aa147
yusuf <yusufpeltek0@gmail.com>
2022-10-14 00:48:47 +0300

    Initial Commit

---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by YUSUFPLTK on 24/12/2022
+//
+
+import Foundation
+
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
